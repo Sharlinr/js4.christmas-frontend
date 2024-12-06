81139c91928fc0153b68b482b002fda8092fd0b2
sharlinrahman <sharlin.rahman91@gmail.com>
2024-12-06 01:14:07 +0100

    homepage fix

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,10 +3,6 @@ import { Link } from 'react-router-dom';
 const HomePage: React.FC = () => {
   return (
     <div className='homepage-wrapper'>
-      <nav className='nav-links'>
-        <Link to='/'>Home</Link> | <Link to='/cart'>Cart</Link> |
-        <Link to='/products'>Products</Link>
-      </nav>
       <h1>Welcome to JulICA</h1>
       <h3>Everyones christmas store</h3>
       <div>
